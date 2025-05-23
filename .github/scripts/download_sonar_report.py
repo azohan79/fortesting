8d28a44380726d2127a8db8afe37af4b28e49bf6
azohan79 <9985585@gmail.com>
2025-05-23 22:04:18 +0300

    ✅ new version pipeline3

---
+++ .github/scripts/download_sonar_report.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+token = os.getenv("SONAR_TOKEN")
+project = "azohan79_fortesting"
+org = "azohan79"
+url = f"https://sonarcloud.io/api/issues/search?organization={org}&componentKeys={project}&ps=500"
+
+resp = requests.get(url, headers={"Authorization": f"Bearer {token}"})
+resp.raise_for_status()
+
+with open("sonar-report.json", "w") as f:
+    f.write(resp.text)
+
+print("✅ sonar-report.json saved.")
